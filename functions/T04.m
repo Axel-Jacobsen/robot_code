b6c94a1cb2a3728a82fd9d4a143a5e14bc5ab06f
Axel-Jacobsen <axelnj44@gmail.com>
2019-11-05 16:10:59 +0100

    seperate T04, DH_Matrix

--- functions/T04.m
+++ functions/T04.m
@@ -41,12 +41,3 @@ function out = T04(q)
     out = T01_dh * T12_dh * T23_dh * T34_dh;
 end
 
-
-function outMatrix = DH_matrix(a, alpha, d, theta)
-    outMatrix = [
-        cos(theta) -sin(theta) * cos(alpha)  sin(theta) * sin(alpha) a * cos(theta);
-        sin(theta)  cos(theta) * cos(alpha) -cos(theta) * sin(alpha) a * sin(theta);
-        0           sin(alpha)               cos(alpha)              d             ;
-        0           0                        0                       1
-    ];
-end
