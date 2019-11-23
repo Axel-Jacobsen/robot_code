60f0fbba926f4bfe751830fb1da9a9b91f5de38b
Axel-Jacobsen <axelnj44@gmail.com>
2019-11-23 17:10:05 +0100

    problem 12 update booboobalooboo

--- problems/problem12.m
+++ problems/problem12.m
@@ -11,7 +11,6 @@ J_eff_4 = (1 / n^2) * max(diag(D4)) + JM;
 f_eff = 2.4e-5;
 KT = 0.17;
 
-
 KP1 = omega^2 * J_eff_1 / KT;
 KP2 = omega^2 * J_eff_2 / KT;
 KP3 = omega^2 * J_eff_3 / KT;
@@ -22,19 +21,14 @@ KD2 = 2*zeta*KP2/omega - f_eff/KT;
 KD3 = 2*zeta*KP3/omega - f_eff/KT;
 KD4 = 2*zeta*KP4/omega - f_eff/KT;
 
-syms s;
+n1 = [KD1  KP1]';
+n2 = [KD2  KP2]';
+n3 = [KD3  KP3]';
+n4 = [KD4  KP4]';
 
-F1 = KP1 + s*KD1;
-F2 = KP2 + s*KD2;
-F3 = KP3 + s*KD3;
-F4 = KP4 + s*KD4;
+d1 = [(1/(KT*n^2)*(JM + max(diag(D1))))  (1/(KT*n^2) * (f_eff + KD1))  KP1]';
+d2 = [(1/(KT*n^2)*(JM + max(diag(D2))))  (1/(KT*n^2) * (f_eff + KD2))  KP2]';
+d3 = [(1/(KT*n^2)*(JM + max(diag(D3))))  (1/(KT*n^2) * (f_eff + KD3))  KP3]';
+d4 = [(1/(KT*n^2)*(JM + max(diag(D4))))  (1/(KT*n^2) * (f_eff + KD4))  KP4]';
 
-N1 = s^2*(1/(KT*n^2)*(JM + max(diag(D1)))) + s*(1/(KT*n^2) * (f_eff + KD1)) + KP1;
-N2 = s^2*(1/(KT*n^2)*(JM + max(diag(D2)))) + s*(1/(KT*n^2) * (f_eff + KD2)) + KP2;
-N3 = s^2*(1/(KT*n^2)*(JM + max(diag(D3)))) + s*(1/(KT*n^2) * (f_eff + KD3)) + KP3;
-N4 = s^2*(1/(KT*n^2)*(JM + max(diag(D4)))) + s*(1/(KT*n^2) * (f_eff + KD4)) + KP4;
-
-T1 = F1 / N1;
-T2 = F2 / N2;
-T3 = F3 / N3;
-T4 = F4 / N4;
+% sim('problem12');
