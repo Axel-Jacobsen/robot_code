2ad217b4c4e66c3de8a3b62668fbb5b6b3d262bd
Axel-Jacobsen <axelnj44@gmail.com>
2019-11-27 21:34:10 +0100

    minor changes; i think this is mathematically correct, but not "DTU Robotics" correct

--- problems/problem12.m
+++ problems/problem12.m
@@ -1,5 +1,6 @@
 problem9;
 
+% 'etc' configurations
 n = 53;
 omega = 15;
 zeta = 1;
@@ -7,6 +8,7 @@ JM = 1320E-7;
 f_eff = 2.4e-5;
 KT = 0.17;
 
+% PD Constants
 KP1 = omega^2 * J_eff(1) / KT;
 KP2 = omega^2 * J_eff(2) / KT;
 KP3 = omega^2 * J_eff(3) / KT;
@@ -17,14 +19,15 @@ KD2 = 2*zeta*KP2/omega - f_eff/KT;
 KD3 = 2*zeta*KP3/omega - f_eff/KT;
 KD4 = 2*zeta*KP4/omega - f_eff/KT;
 
-n1 = [KD1  KP1];
-n2 = [KD2  KP2];
-n3 = [KD3  KP3];
-n4 = [KD4  KP4];
+% Transfer Function Numerators (ni) and Denomenators (di)
+n1 = 2 * KT * n * [KD1  KP1];
+n2 = 2 * KT * n * [KD2  KP2];
+n3 = 2 * KT * n * [KD3  KP3];
+n4 = 2 * KT * n * [KD4  KP4];
 
-d1 = [(1/(KT*n^2)*(JM + sup_q(1)))  (1/(KT*n^2) * (f_eff + KD1))  KP1];
-d2 = [(1/(KT*n^2)*(JM + sup_q(2)))  (1/(KT*n^2) * (f_eff + KD2))  KP2];
-d3 = [(1/(KT*n^2)*(JM + sup_q(3)))  (1/(KT*n^2) * (f_eff + KD3))  KP3];
-d4 = [(1/(KT*n^2)*(JM + sup_q(4)))  (1/(KT*n^2) * (f_eff + KD4))  KP4];
+d1 = [(JM*n + (1/n)*sup_q(1))  (f_eff/n + KT*KD1*n)  KT*n*KP1];
+d2 = [(JM*n + (1/n)*sup_q(2))  (f_eff/n + KT*KD2*n)  KT*n*KP2];
+d3 = [(JM*n + (1/n)*sup_q(3))  (f_eff/n + KT*KD3*n)  KT*n*KP3];
+d4 = [(JM*n + (1/n)*sup_q(4))  (f_eff/n + KT*KD4*n)  KT*n*KP4];
 
 sim('simulink_problem12');
